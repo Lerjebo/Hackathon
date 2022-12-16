2a2c2d8d7e0f3d97253a56ea0ca96e4f4ca3a37e
TobiasCarl <tobias.carlsson@kits.se>
2022-12-16 14:59:44 +0100

    Added structure, design and some challenges

--- backend/handler.js
+++ backend/handler.js
@@ -1,18 +1,351 @@
-'use strict';
+'use strict'
+const AWS = require('aws-sdk')
+// Set region
+// Set the AWS Region.
 
-module.exports.hello = async (event) => {
+AWS.config.update({ region: 'eu-west-1' })
+const docClient = new AWS.DynamoDB.DocumentClient({ region: 'eu-west-1' })
+var dynamodb = new AWS.DynamoDB({ apiVersion: '2012-08-10' })
+const characters =
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+
+function generateString (length) {
+  let result = ' '
+  const charactersLength = characters.length
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength))
+  }
+
+  return result
+}
+
+module.exports.getChallenge = async (event, context) => {
+  var params = {
+    TableName: 'HackathonChallenges',
+    Select: 'ALL_ATTRIBUTES'
+  }
+  let resp = await dynamodb.scan(params).promise()
   return {
     statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: 'Go Serverless v1.0! Your function executed successfully!',
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(resp)
+  }
+}
 
-  // Use this code if you don't use the http event with the LAMBDA-PROXY integration
-  // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+const s3 = new AWS.S3()
+
+module.exports.upLoadInput = async (event, context) => {
+  let data = JSON.parse(event['body'])
+  let teamName = data['teamName']
+
+  let resp = await s3
+    .putObject({
+      Bucket: 'codepage-hackathon-hackathon-inputfilesbucket-34eho671ijhs',
+      Key: 'YOUR_FILE_NAME.txt',
+      Body: 'formData'
+    })
+    .promise()
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(resp)
+  }
+}
+
+module.exports.getInput = async (event, context) => {
+  let values = event['path'].split('/')
+  let inputId = values[3]
+
+  console.log(inputId)
+
+  let resp = await s3.getSignedUrl('getObject', {
+    Bucket: 'codepage-hackathon-hackathon-inputfilesbucket-34eho671ijhs',
+    Key: inputId + '.txt',
+    Expires: 1000
+  })
+  console.log(resp)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: resp
+  }
+}
+
+module.exports.getTeam = async (event, context) => {
+  let values = event['path'].split('/')
+  let tempS = ''
+  var arrayLength = values.length
+  for (var i = 3; i < arrayLength; i++) {
+    tempS += decodeURIComponent(values[i])
+  }
+
+  let teamId = decodeURIComponent(values[3])
+  console.log('Val')
+  console.log(values)
+
+  console.log(tempS)
+  var params = {
+    TableName: 'HackathonTeams',
+    Key: {
+      PK: {
+        S: teamId
+      }
+    }
+  }
+  let resp = await dynamodb.getItem(params).promise()
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(resp)
+  }
+}
+
+async function getChallenges(teamName){
+
+  
+  //CHALLENGE 0
+  var lengthOfLongestSubstring = function ([string]) {
+    var max = 0,
+      current_string = '',
+      i,
+      char,
+      pos
+
+    for (i = 0; i < string.length; i += 1) {
+      char = string.charAt(i)
+      pos = current_string.indexOf(char)
+      if (pos !== -1) {
+        current_string = current_string.substr(pos + 1)
+      }
+      current_string += char
+      max = Math.max(max, current_string.length)
+    }
+    return max
+  }
+  //CHALLENGE 1
+  var strStr = function ([haystack, needle]) {
+    return haystack.indexOf(needle)
+  }
+  //CHALLENGE 2
+  var minJumps = function([steps]) {
+    const minJumps = new Array(steps.length).fill(Infinity);
+    minJumps[0] = 0;
+    for (let i = 0; i < steps.length; i++) {
+      for (let j = 1; j <= steps[i]; j++) {
+        if (i + j < steps.length) {
+          minJumps[i + j] = Math.min(minJumps[i + j], minJumps[i] + 1);
+        }
+      }
+    }
+  
+    return minJumps[steps.length - 1];
+  }
+  //CHALLENGE 3
+  var countPalindromes = function ([s]) {
+    let n = s.length;
+    let count = 0;
+    for (let i = 0; i < n; i++) {
+      let palindromeOdd = getPalindromeLength(s, i, i);
+      let palindromeEven = getPalindromeLength(s, i, i + 1);
+      count += palindromeOdd + palindromeEven;
+    }
+    return count;
+  }
+  var getPalindromeLength= function (s, left, right) {
+    let count = 0;
+    while (left >= 0 && right < s.length && s[left] === s[right]) {
+      count++;
+      left--;
+      right++;
+    }
+    return count;
+  }
+  //CHALLENGE 4
+  var sumOfSquares= function([numbers]) {
+    let sum = 0;
+    for (let i = 0; i < numbers.length; i++) {
+      if (numbers[i] % 2 === 0) {
+        sum += numbers[i] ** 2;
+      }
+    }
+    return sum;
+  }
+  //CHALLENGE 4
+  var replaceVowels = function([str]) {
+    let vowels = 'aeiouAEIOU';
+    let newStr = '';
+    for (let i = 0; i < str.length; i++) {
+      if (vowels.indexOf(str[i]) !== -1) {
+        newStr += '*';
+      } else {
+        newStr += str[i];
+      }
+    }
+    return newStr;
+  }
+
+
+  function generateRandomInputVow(length) {
+    let input = '';
+    for (let i = 0; i < length; i++) {
+      input += String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+    }
+    return input;
+  }
+
+
+  //ADD challenge functions below.
+  let challengeFunctions = [
+    ([inp]) => lengthOfLongestSubstring([inp]),
+    ([inp1, inp2]) => strStr([inp1, inp2]),
+    ([inp]) => minJumps([inp]),
+    ([inp]) => countPalindromes([inp]),
+    ([inp]) => sumOfSquares([inp]),
+    ([inp]) => replaceVowels([inp])
+  ]
+
+  //ADD challenge inputs below.
+  let generateRandomInput = [
+    () => [generateString(100)],
+    () => {
+      let randStr = generateString(25)
+      let substr = randStr.substr(
+        Math.floor(Math.random() * (randStr.length - 4)),
+        4
+      )
+      return [randStr, substr]
+    },
+    () => [Array.from({length: 500}, () => Math.floor(Math.random() * 6)+1)],
+    () => [generateString(100)],
+    () => [Array.from({length: 500}, () => Math.floor(Math.random() * 100)+1)],
+    () => [generateRandomInputVow(100)]
+  ]
+
+  let teamInput = []
+  for (let i = 0; i < challengeFunctions.length; i++) {
+    let tempInp = generateRandomInput[i]()
+    teamInput.push(tempInp)
+    await s3
+      .putObject({
+        Bucket: 'codepage-hackathon-hackathon-inputfilesbucket-34eho671ijhs',
+        Key: teamName + 'ch' + i + '.txt',
+        Body: tempInp.toString()
+      })
+      .promise()
+  }
+
+  let expectedOutput = []
+  for (let i = 0; i < challengeFunctions.length; i++) {
+    expectedOutput.push(challengeFunctions[i](teamInput[i]))
+  }
+
+  const challengeStatusMap = {}
+  for (let i = 0; i < challengeFunctions.length; i++) {
+    challengeStatusMap[i] = false
+  }
+  const challengeOutputMap = {}
+  for (let i = 0; i < challengeFunctions.length; i++) {
+    challengeOutputMap[i] = expectedOutput[i]
+  }
+  return [challengeStatusMap, challengeOutputMap]
+}
+
+module.exports.postTeam = async (event, context) => {
+  let data = JSON.parse(event['body'])
+  let teamName = data['teamName']
+
+  const challengeInfo = await getChallenges(teamName);
+
+  var params = {
+    TableName: 'HackathonTeams',
+    Item: {
+      PK: teamName,
+      ChallengeStatus: challengeInfo[0],
+      ChallengeExpectedOutputs: challengeInfo[1],
+    }
+  }
+  let resp = await docClient.put(params).promise()
+  console.log('RESP')
+  console.log(resp)
+  return {
+    statusCode: 200,
+    headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
+    body: JSON.stringify(resp)
+  }
+}
+module.exports.updateTeam = async (event, context) => {
+  console.info(event)
+  let data = JSON.parse(event['body'])
+  console.info(data)
+
+  let teamName = data['teamName']
+
+  var params = {
+    TableName: 'HackathonTeams',
+    Key: {
+      PK: {
+        S: teamName
+      }
+    }
+  }
+  let resp = await dynamodb.getItem(params).promise()
+  console.log(resp)
+
+  let challengeId = data['challengeId']
+  let challengeStatus = data['challengeStatus']
+  const challengeStatusMap = {}
+
+  for (const [key, value] of Object.entries(resp.Item.ChallengeStatus.M)) {
+    console.log(value)
+    if (key == challengeId) {
+      challengeStatusMap[key] = challengeStatus
+    } else {
+      challengeStatusMap[key] = value.BOOL
+    }
+  }
+
+  console.info(challengeStatusMap)
+
+  let updExp = 'set ChallengeStatus =:updateValue'
+  var params = {
+    TableName: 'HackathonTeams',
+    Key: {
+      PK: teamName
+    },
+    UpdateExpression: updExp,
+    ExpressionAttributeValues: {
+      ':updateValue': challengeStatusMap
+    }
+  }
+  console.log('HERE')
+  try {
+    await docClient.update(params).promise()
+    return {
+      statusCode: 200,
+      body: JSON.stringify(
+        {
+          message: 'Sucessfully uploaded.',
+          input: event
+        },
+        null,
+        2
+      ),
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      }
+    }
+  } catch (err) {
+    console.log(err)
+    return err
+  }
+}
---
+++ frontend/src/App.tsx
@@ -0,0 +1,434 @@
+import React from 'react'
+import './App.css'
+import axios from 'axios'
+import { useContext, useState, useEffect } from 'react'
+const backGround = require('./deskBackground.JPG')
+function App () {
+  const [teamInformation, setTeamInformation] = useState({
+    PK: '',
+    ChallengeExpectedOutputs: new Map(),
+    ChallengeInputs: new Map(),
+    ChallengeStatus: new Map()
+  })
+
+  const [challengeInfo, setChallengeInfo] = useState(() => new Map())
+  const [challengeInfoKeys, setChallengeInfoKeys] = useState<string[]>([])
+  const [currentTeam, setCurrentTeam] = useState('')
+  const [queueLock, setQueueLock] = useState(false)
+  const [currentChallenge, setCurrentChallenge] = useState(0)
+  const [inputs, setInputs] = useState({ teamName: '', answer: '' })
+
+  const [loggedIn, setloggedIn] = useState(false)
+
+  const API_BASE_URL = 'https://6b9dfg8pp3.execute-api.eu-west-1.amazonaws.com'
+  const getChallengesFromDatabase = () => {
+    let addr = API_BASE_URL + '/Hackathon/getChallenge/getChallenges'
+
+    return axios
+      .get(addr)
+      .then(res => [res.data, res.status])
+      .catch(reason => [reason, reason])
+  }
+
+  const getTeamInputFromDatabase = (inputId: any) => {
+    let addr = API_BASE_URL + '/Hackathon/getInput/inputId' + `/${inputId}`
+    setQueueLock(true)
+    return axios
+      .get(addr)
+      .then(res => [res.data, res.status])
+      .catch(reason => [reason, reason])
+  }
+
+  const getTeamFromDatabase = (teamId: any) => {
+    let addr = API_BASE_URL + '/Hackathon/getTeam/teamId' + `/${teamId}`
+    setQueueLock(true)
+    return axios
+      .get(addr)
+      .then(res => [res.data, res.status])
+      .catch(reason => [reason, reason])
+  }
+  const updateTeamInDatabase = (
+    teamName: any,
+    challengeId: any,
+    challengeStatus: any
+  ) => {
+    let addr = API_BASE_URL + '/Hackathon/updateTeam'
+    setQueueLock(true)
+    let updateData = {
+      teamName: teamName,
+      challengeId: challengeId,
+      challengeStatus: challengeStatus
+    }
+    return axios
+      .post(addr, updateData)
+      .then(res => console.log(res))
+      .catch(reason => console.log(reason))
+  }
+
+  useEffect(() => {}, [JSON.stringify(teamInformation)])
+
+  useEffect(() => {
+    if (Object.keys(challengeInfo).length == 0 && currentTeam != '') {
+      getChallengesFromDatabase().then(data => {
+        const [challengeData, status] = data
+        Object.keys(challengeData['Items']).map(key => {
+          getTeamInputFromDatabase(
+            currentTeam + 'ch' + challengeData['Items'][key].PK.S
+          ).then(data2 => {
+            challengeInfo.set(challengeData['Items'][key].PK.S, [
+              challengeData['Items'][key].Description.S,
+              data2['0']
+            ])
+
+            if (!challengeInfoKeys.includes(challengeData['Items'][key].PK.S)) {
+              challengeInfoKeys.push(challengeData['Items'][key].PK.S)
+            }
+            if (
+              challengeInfo.size == Object.keys(challengeData['Items']).length
+            ) {
+              setloggedIn(true)
+            }
+          })
+        })
+      })
+    }
+  }, [JSON.stringify(currentTeam)])
+
+  const onClickChallenge = (id: any) => {
+    setCurrentChallenge(id)
+    inputs['answer'] = ''
+    setInputs(inputs)
+  }
+  const handleChange = (event: any) => {
+    const name = event.target.name
+    const value = event.target.value.replace(/[^a-zA-Z0-9-*]/g, '')
+    setInputs(values => ({ ...values, [name]: value }))
+  }
+
+  const handleSubmit = (event: any) => {
+    event.preventDefault()
+
+    let correctAnswer = teamInformation.ChallengeExpectedOutputs.get(
+      currentChallenge.toString()
+    ).N
+    correctAnswer =
+      typeof correctAnswer == 'undefined'
+        ? teamInformation.ChallengeExpectedOutputs.get(
+            currentChallenge.toString()
+          ).S
+        : correctAnswer
+    if (inputs.answer == correctAnswer) {
+      let newInfo = teamInformation.ChallengeStatus.get(
+        currentChallenge.toString()
+      )
+      newInfo.BOOL = true
+      teamInformation.ChallengeStatus.set(currentChallenge.toString(), newInfo)
+      setTeamInformation(teamInformation)
+
+      updateTeamInDatabase(currentTeam, currentChallenge, true)
+      const element = window.document.getElementById('span' + currentChallenge)
+      if (element) {
+        element.style.visibility = 'visible'
+        alert('Correct!')
+      }
+    } else {
+      setInputs({ ...inputs, answer: '' })
+      alert('Incorrect, try again!')
+    }
+  }
+
+  const getTeamFromDataBaseAndSave = () => {
+    getTeamFromDatabase(inputs.teamName).then(data => {
+      const [teamData, status] = data
+      if (Object.keys(teamData).length > 0) {
+        for (const [key, value] of Object.entries(
+          teamData.Item.ChallengeStatus.M
+        )) {
+          teamInformation.ChallengeStatus.set(key, value)
+        }
+        for (const [key, value] of Object.entries(
+          teamData.Item.ChallengeExpectedOutputs.M
+        )) {
+          teamInformation.ChallengeExpectedOutputs.set(key, value)
+        }
+        setTeamInformation(teamInformation)
+        setCurrentTeam(inputs.teamName)
+      } else {
+        let addr = API_BASE_URL + '/Hackathon/postTeam'
+        setQueueLock(true)
+        let updateData = {
+          teamName: inputs.teamName,
+          numberChallenges: 10
+        }
+        return axios
+          .post(addr, updateData)
+          .then(res => {
+            setQueueLock(false)
+            getTeamFromDataBaseAndSave()
+          })
+          .catch(reason => alert('Could not add team, try again!'))
+      }
+    })
+  }
+  if (currentTeam == '') {
+    const handleSubmitTeamName = (event: any) => {
+      event.preventDefault()
+      getTeamFromDataBaseAndSave()
+    }
+
+    return (
+      <div
+        id='ChallengeContainer'
+        className='loginContainer'
+        style={{ width: '100vw', display: 'flex' }}
+      >
+        <div
+          className='paper'
+          style={{ width: '60%', margin: 'auto', display: 'flex' }}
+        >
+          <div className='pattern' style={{ width: '100%' }}>
+            <div>
+              <form
+                style={{
+                  margin: 'auto',
+                  display: 'inline-grid',
+                  marginTop: '10%',
+                  width: '100%'
+                }}
+                onSubmit={handleSubmitTeamName}
+              >
+                <label
+                  style={{
+                    margin: 'auto',
+                    width: '60%',
+                    borderRadius: '100px',
+                    textAlign: 'center',
+                    fontSize: 'xx-large'
+                  }}
+                >
+                  Teamname
+                </label>
+                <br />
+
+                <input
+                  required
+                  style={{
+                    margin: 'auto',
+
+                    width: '60%',
+                    borderRadius: '100px',
+                    textAlign: 'center',
+                    fontSize: 'xx-large'
+                  }}
+                  id='teamName'
+                  pattern='^[a-zA-Z0-9]+$'
+                  type='text'
+                  name='teamName'
+                  value={inputs.teamName || ''}
+                  onChange={handleChange}
+                />
+
+                <br />
+
+                <br />
+                <input
+                  className='inputOnHover'
+                  style={{
+                    margin: 'auto',
+
+                    marginTop: '20px',
+                    width: '60%',
+                    borderRadius: '100px',
+                    textAlign: 'center',
+                    fontSize: 'xx-large',
+                    background: 'rgb(139, 161, 125)',
+                    marginBottom: '20px'
+                  }}
+                  type='submit'
+                  value={'Log in'}
+                />
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  } else if (!loggedIn) {
+    return (
+      <div
+        className='Container'
+        style={{
+          width: '100%',
+          height: '100vh',
+          display: 'flex'
+        }}
+      >
+        <div className='lds-roller'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+      </div>
+    )
+  } else {
+    challengeInfoKeys.sort()
+
+    // Print the sorted array
+    return (
+      <>
+        <div
+          className='Container'
+          style={{
+            width: '100%',
+            height: '5vh',
+            display: 'flex'
+          }}
+        >
+          <div
+            className='contentPattern'
+            style={{ margin: 'auto', width: '100%', display: 'flex' }}
+          >
+            <div
+              style={{
+                marginLeft: '5%',
+                color: 'black',
+                background: '#8BA17D',
+                display: 'flex',
+                border: '2px solid black'
+              }}
+            >
+              <a style={{ margin: 'auto' }}> {'Team name: ' + currentTeam} </a>
+            </div>
+            <input
+              onClick={() => window.location.reload()}
+              className='inputOnHover'
+              style={{
+                borderRadius: '100px',
+                textAlign: 'center',
+                margin: 'auto',
+                marginRight: '5%',
+
+                fontSize: 'xx-large',
+                background: 'rgb(139, 161, 125)'
+              }}
+              type='submit'
+              value={'Switch team'}
+            />{' '}
+          </div>
+        </div>
+        <div id='Container' className='Container' style={{ height: '95vh' }}>
+          <div className='paper' style={{ width: '30%' }}>
+            <div className='pattern'>
+              <div className='contentPattern'>
+                {challengeInfoKeys.map(key => (
+                  <>
+                    <a
+                      href='#'
+                      className='ChallengeTextStyle'
+                      onClick={() => onClickChallenge(key)}
+                      style={{ textDecoration: 'None' }}
+                    >
+                      {' '}
+                      {'Challenge: ' + key}
+                    </a>
+
+                    <span
+                      id={'span' + key}
+                      style={{
+                        visibility:
+                          teamInformation.ChallengeStatus.get(key).BOOL == true
+                            ? 'visible'
+                            : 'hidden',
+                        color: 'green',
+                        marginLeft: '10px'
+                      }}
+                    >
+                      &#10003;
+                    </span>
+                    <br />
+                  </>
+                ))}
+              </div>
+            </div>
+          </div>{' '}
+          <div className='paper' style={{ width: '60%' }}>
+            <div className='pattern'>
+              <div
+                className='contentPattern'
+                style={{ position: 'relative', height: '100%' }}
+              >
+                <div style={{ marginLeft: '7%', marginRight: '3%' }}>
+                  {'Challenge ' + currentChallenge}
+                  <br />
+                  <br />
+                  {challengeInfo.get(currentChallenge.toString())[0]}
+
+                  <br />
+                  <br />
+                  <a
+                    target='_blank'
+                    href={challengeInfo.get(currentChallenge.toString())[1]}
+                  >
+                    Download input file
+                  </a>
+                </div>
+                <div
+                  style={{ position: 'absolute', bottom: '10%', width: '100%' }}
+                >
+                  <form onSubmit={handleSubmit}>
+                    <label
+                      style={{
+                        width: '60%',
+                        borderRadius: '100px',
+                        textAlign: 'center',
+                        fontSize: 'xx-large',
+                        background: 'white'
+                      }}
+                    >
+                      Answer
+                    </label>
+                    <br />
+                    <input
+                      required
+                      style={{
+                        width: '60%',
+                        borderRadius: '100px',
+                        textAlign: 'center',
+                        fontSize: 'xx-large'
+                      }}
+                      type='text'
+                      name='answer'
+                      value={inputs.answer || ''}
+                      onChange={handleChange}
+                    />
+                    <br />
+                    <input
+                      className='inputOnHover'
+                      style={{
+                        marginTop: '20px',
+                        width: '60%',
+                        borderRadius: '100px',
+                        textAlign: 'center',
+                        fontSize: 'xx-large',
+                        background: '#8BA17D'
+                      }}
+                      type='submit'
+                      value={'Submit'}
+                    />
+                  </form>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </>
+    )
+  }
+}
+
+export default App
